352299edb36dcd634c387b9f012c28f1e23b3d89
JacobIChr-Park <158213913+JacobIChr-Park@users.noreply.github.com>
2024-02-19 21:12:32 +0000

    changed hello_world

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-import computing_with_formulas add_two_numbers
-birth_month = 5
-birth_day = 8
-birth_sum = birth_month + birth_day
-print(birth_sum)
+import computing_with_formulas.print_hello_world
+
   
