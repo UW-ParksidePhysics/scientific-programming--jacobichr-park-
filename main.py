d3038fb2b88ddfe07a995ea27dbce1a0d77e5ca2
Jacob Christensen <chris225@rangers.uwp.edu>
2024-02-19 15:57:13 -0600

    cleared main

--- main.py
+++ main.py
@@ -2,8 +2,3 @@ import computing_with_formulas.print_hello_world
 import numpy as np
 
 
-import matplotlib.pyplot as plt
-
-plt.plot(np.linspace(0,10,100),np.sin(np.linspace(0,10,100)))
-
-plt.show()
